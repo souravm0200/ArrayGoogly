595a532dd37855b4482767a7e26bf610236e8a55
SOURAV MANDAL <sourav0200@gmail.com>
2023-04-10 21:05:29 +0600

    C++ file correction done and its completed

--- ArrayGoogly.cpp
+++ ArrayGoogly.cpp
@@ -1,38 +1,32 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-vector<int> product_except_self(vector<int>& nums) {
+vector<int> arrayGoogly(vector<int>& nums) {
     int n = nums.size();
-    vector<int> left_products(n);
-    vector<int> right_products(n);
-    vector<int> output(n);
+    vector<int> output(n, 1);
 
     // compute products of all elements to the left of each index
-    left_products[0] = 1;
-    for (int i = 1; i < n; i++) {
-        left_products[i] = left_products[i-1] * nums[i-1];
+    int leftProduct = 1;
+    for (int i = 0; i < n; i++) {
+        output[i] *= leftProduct;
+        leftProduct *= nums[i];
     }
 
     // compute products of all elements to the right of each index
-    right_products[n-1] = 1;
-    for (int i = n-2; i >= 0; i--) {
-        right_products[i] = right_products[i+1] * nums[i+1];
-    }
-
-    // compute final output array
-    for (int i = 0; i < n; i++) {
-        output[i] = left_products[i] * right_products[i];
+    int rightProduct = 1;
+    for (int i = n-1; i >= 0; i--) {
+        output[i] *= rightProduct;
+        rightProduct *= nums[i];
     }
 
     return output;
 }
 
-
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5};
-    vector<int> output = product_except_self(nums);
+    vector<int> output = arrayGoogly(nums);
 
     for (int i = 0; i < output.size(); i++) {
         cout << output[i] << " ";
